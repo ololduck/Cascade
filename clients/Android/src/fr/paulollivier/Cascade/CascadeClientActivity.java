612eb0c6f2ad402a98ccd4c73e10d8d7df85e2b6
Paul Ollivier <paul.ollivier@druil.net>
2012-06-03 19:51:50 +0200

    Intents now work

--- clients/Android/src/fr/paulollivier/Cascade/CascadeClientActivity.java
+++ clients/Android/src/fr/paulollivier/Cascade/CascadeClientActivity.java
@@ -1,7 +1,13 @@
 package fr.paulollivier.Cascade;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CascadeClientActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +15,17 @@ public class CascadeClientActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button savebutton = (Button) findViewById(R.id.savebutton);
+        savebutton.setOnClickListener(new OnClickListener(){
+        	public void onClick(View v){
+        		// save preferences
+        		SharedPreferences sharedpref = getSharedPreferences("Cascade", Activity.MODE_PRIVATE);
+        		SharedPreferences.Editor editor = sharedpref.edit();
+        		editor.putString("serv_addr", (String) ((TextView) findViewById(R.id.et_serv_addr)).getText());
+        		editor.commit();
+        		Toast.makeText(getBaseContext(), R.string.saved_preferences_popup, Toast.LENGTH_LONG).show();
+        	}
+        });
     }
 }
