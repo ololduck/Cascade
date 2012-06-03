4d1a664d49979a8ef1d5a99c0a3e2e45f3e0eeab
Paul Ollivier <paul.ollivier@druil.net>
2012-06-03 15:19:04 +0200

    Woops. Added files this time. Begun main layout creation

---
+++ clients/Android/src/fr/paulollivier/Cascade/CascadeClientActivity.java
@@ -0,0 +1,13 @@
+package fr.paulollivier.Cascade;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CascadeClientActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
